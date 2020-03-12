2970c9bfe5ea81d0280d2eb47ec437d5497d5875
xuwww <15270739190@163.com>
2020-03-12 12:47:19 +0800

    HelloController

---
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public Object greeting(){
+        return "Hello!";
+    }
+}
